6f267f2fea5955128944ca1361d0e23d54370091
Mackenzie Willard <mtw2935@rit.edu>
2017-08-07 21:05:34 -0400

    Adding more base information.

---
+++ include/header.inc.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <script src="../jquery/jquery-3.2.1.js" type="text/javascript"></script>
+</head>
+        <?php
+        // put your code here
+        ?>
