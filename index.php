4e2827b7c45b55208937072bd1ae365d3aa7531a
Mackenzie Willard <mtw2935@rit.edu>
2017-08-09 17:06:56 -0400

    Started first connections
    Started a test connection to the database to make sure that it works.
    Will move information to an area outside the main folder for safety.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!--
+<!---->
 
 <html>
     <head>
@@ -8,7 +8,31 @@
     </head>
     <body>
         <?php
-        // put your code here
+            echo "<p>Hello</p>";
+            $servername = "localhost";
+            $username = "root";
+            $password = "root";
+            $dbname = "animated_sign";
+            
+            //creating the connection
+            $conn = new mysqli($servername,$username,$password,$dbname);
+            //checking the connection
+            if ($conn->connect_error){
+                die("Connection failed: " . $conn->connect_error);
+                echo "" . $conn->connect_error;
+            }
+            
+            $sql = "SELECT * FROM users";
+            
+            $result = $conn->query($sql);
+            
+            if($result->num_rows > 0){
+                //output data of each row
+                while($row = $result->fetch_assoc()){
+                    echo "id: " . $row["id"] . "<br>";
+                }
+            }
+            
         ?>
     </body>
 </html>
