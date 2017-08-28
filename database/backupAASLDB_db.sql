927f8141ac8e3ef2b086da2c751b05e83a3d61df
Mackenzie Willard <mtw2935@rit.edu>
2017-08-28 19:14:23 -0400

    DB Backup 4

--- database/backupAASLDB_db.sql
+++ database/backupAASLDB_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 25, 2017 at 03:00 AM
+-- Generation Time: Aug 29, 2017 at 01:10 AM
 -- Server version: 5.6.35
 -- PHP Version: 7.1.6
 
