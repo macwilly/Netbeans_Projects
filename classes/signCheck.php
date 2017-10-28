2c523ad6720e8525017a64ebdd5b84c5b0e7b36f
Mackenzie Willard <mtwillard29@gmail.com>
2017-10-28 15:49:52 -0400

    more checks

--- classes/signCheck.php
+++ classes/signCheck.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+$gloss = finter_input(INPUT_POST,'inputgloss',FILTER_SANITIZE_STRING,FILTER_SANITIZE_ENCODED);
+$english = filter_input(INPUT_POST,'inputenglish',FILTER_SANITIZE_STRING,FILTER_SANITIZE_ENCODED);
 $asllvdLink = filter_input(INPUT_POST,'inputasllvd');
+$fish = filter_input(INPUT_POST, 'optionsFinished',FILTER_SANITIZE_NUMBER_INT);
+$hand = filter_input(INPUT_POST, 'optionsHandedness',FILTER_SANITIZE_NUMBER_INT);
 
-echo $asllvdLink;
+
