82ded3540b265642c1b498f92b9a1a2426a738de
Mackenzie Willard <mtwillard29@gmail.com>
2017-10-30 04:27:34 -0400

    DB Backup 9

--- database/backupAASLDB_db.sql
+++ database/backupAASLDB_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.2
+-- version 4.7.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 19, 2017 at 01:46 AM
+-- Generation Time: Oct 30, 2017 at 09:25 AM
 -- Server version: 5.6.35
--- PHP Version: 7.1.6
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -25,6 +25,25 @@ CREATE TABLE `attributes` (
   `attribute` int(11) NOT NULL,
   `description` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attribute_list`
+--
+
+CREATE TABLE `attribute_list` (
+  `name` varchar(30) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `active` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `attribute_list`
+--
+
+INSERT INTO `attribute_list` (`name`, `description`, `active`) VALUES
+('Is in study', 'This is for signs that are in studies. The description will be the study', 1);
 
 -- --------------------------------------------------------
 
@@ -59,7 +78,7 @@ CREATE TABLE `hand_shape` (
 --
 
 INSERT INTO `hand_shape` (`id`, `description`, `embr_description`, `image`, `active`) VALUES
-(0, '', '', '', 0),
+(0, 'test99', 'testss', 'na', 1),
 (1, 'A', '', 'na', 1),
 (2, 'B', '', 'na', 1),
 (3, 'B-xd', '', 'na', 1),
@@ -127,7 +146,7 @@ INSERT INTO `hand_shape` (`id`, `description`, `embr_description`, `image`, `act
 (65, 'bent-Horns', '', 'na', 1),
 (66, 'O/2-Horns', '', 'na', 1),
 (67, 'RLxd', '', 'na', 1),
-(68, '1', '', 'na', 1),
+(68, '1', '1', 'na', 1),
 (69, 'bent-1', '', 'na', 1),
 (70, '3', '', 'na', 1),
 (71, 'crvd-3', '', 'na', 1),
@@ -166,18 +185,18 @@ CREATE TABLE `related_sign` (
 
 CREATE TABLE `sign` (
   `id` int(10) UNSIGNED NOT NULL,
-  `ember_xml` blob NOT NULL COMMENT 'This is the information that the animated sign is made of.',
+  `embr_xml` varchar(8000) NOT NULL COMMENT 'This is the information that the animated sign is made of.',
   `gloss` varchar(30) NOT NULL,
-  `dominate_start_HS` varchar(20) NOT NULL,
-  `dominate_end_HS` varchar(20) NOT NULL,
-  `nondominate_start_HS` varchar(20) NOT NULL,
+  `dominant_start_HS` varchar(20) DEFAULT NULL,
+  `dominant_end_HS` varchar(20) DEFAULT NULL,
+  `nondominant_start_HS` varchar(20) DEFAULT NULL,
   `handedness` int(11) NOT NULL COMMENT 'Is the sign one handed or two handed 1 = 1h 2 = 2h',
-  `nondominate_end_HS` varchar(20) NOT NULL,
+  `nondominant_end_HS` varchar(20) DEFAULT NULL,
   `english_meaning` varchar(30) NOT NULL,
   `start_photo` char(50) NOT NULL,
   `end_photo` char(50) NOT NULL,
   `finished` int(11) NOT NULL COMMENT 'Is the sign finished 0=No 1=Yes',
-  `asllvd_link` varchar(100) NOT NULL
+  `asllvd_link` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -216,7 +235,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL COMMENT 'Unique identifier for all users',
   `username` varchar(10) NOT NULL,
   `password` varbinary(20) NOT NULL COMMENT 'Password is encrypted with AES',
-  `security_level` int(11) NOT NULL COMMENT '0 = Admin 1 = user',
+  `security_level` int(11) NOT NULL COMMENT '2 = Admin 1 = user',
   `first_name` char(20) NOT NULL,
   `last_name` char(20) NOT NULL,
   `active` int(11) NOT NULL COMMENT '0 = Inactive 1 = Active',
@@ -228,8 +247,15 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `security_level`, `first_name`, `last_name`, `active`, `email`) VALUES
-(2, 'mtw2935', 0x9ed38d8c7e95f75ad5b672d6e7904853, 0, 'Mackenzie', 'Willard', 1, 'mtwillard29@gmail.com'),
-(3, 'test', 0x8ff532d3b3a8fbe0bef61eac4078a86e, 1, 'John', 'Testington', 1, 'jtestington@rit.edu');
+(2, 'mtw2935', 0x9ed38d8c7e95f75ad5b672d6e7904853, 3, 'Mackenzie', 'Willard', 1, 'mtwillard29@gmail.com'),
+(3, 'jxt1234', 0x3f3f32d3b33f3f3f3f3f40783f6e, 1, 'John', 'Testington', 1, 'jtestington@rit.edu'),
+(4, 'test', 0x74657374, 1, 'firstTest', 'lastTest', 1, 'acb123@email.com'),
+(5, 'chicken', 0x3f3f3f3f363f486e3f3f3f533f3f3f3f733f, 2, 'Doug', 'lname', 1, 'email@email.com'),
+(6, 'bbk1234', 0x503f3f3f3f21523f3f333f653f, 1, 'Bri', 'Kelly', 1, 'bbk1234@rit.edu'),
+(7, 'mgs4321', 0x4145535f454e4352595054282744756e64657227, 2, 'Michael', 'Scott', 1, 'mgs432@df.com'),
+(8, 'jth1234', 0x636c6b3f3f3f523f3f7d3f3f3f3f3f, 1, 'Jim', 'Halpert', 1, 'jth1234@df.com'),
+(9, 'dba123', 0x3f4a7d3f453f683f367d3f3f3f, 2, 'David', 'Atten', 1, 'dba123@rit.edu'),
+(10, 'mphics', 0x727b743f3f5b3fdba94e36603f78, 3, 'Matt', 'Huenerfauth', 1, 'matt.huenerfauth@rit.edu');
 
 --
 -- Indexes for dumped tables
@@ -240,6 +266,12 @@ INSERT INTO `users` (`id`, `username`, `password`, `security_level`, `first_name
 --
 ALTER TABLE `attributes`
   ADD PRIMARY KEY (`sign_id`,`attribute`);
+
+--
+-- Indexes for table `attribute_list`
+--
+ALTER TABLE `attribute_list`
+  ADD PRIMARY KEY (`name`);
 
 --
 -- Indexes for table `auditTrail`
@@ -291,4 +323,4 @@ ALTER TABLE `sign`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier for all users', AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique identifier for all users', AUTO_INCREMENT=11;
