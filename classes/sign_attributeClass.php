dd1995f9737e93f1a79110f82749fd567d17e142
Mackenzie Willard <mtwillard29@gmail.com>
2017-10-30 18:32:18 -0400

    new class

---
+++ classes/sign_attributeClass.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of attributeListClass
+ *
+ * @author mackenzie
+ */
+class sign_attributeClass {
+    
+    private $_name;
+    private $_description;
+}
