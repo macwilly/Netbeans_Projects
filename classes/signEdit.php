22279222581fa4e541064185f2ff6e9bf44e9420
Mackenzie Willard <mtwillard29@gmail.com>
2017-11-11 10:19:55 -0500

    edit part 2

--- classes/signEdit.php
+++ classes/signEdit.php
@@ -10,40 +10,33 @@ include '../function/getRelatedSigns.php';
 $inEd = filter_input(INPUT_POST, 'insertEdit');
 $sentSign = filter_input(INPUT_POST, 'startSign');
 $user = $_SESSION['userId'];
+$embr = filter_input(INPUT_POST, 'embrtext');
+$dbRelated = getRelatedSignArray($sentSign);
+
+$gloss = filter_input(INPUT_POST, 'inputgloss', FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED);
+$english = filter_input(INPUT_POST, 'inputenglish', FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED);
+$asllvdLink = filter_input(INPUT_POST, 'inputasllvd');
+$fish = filter_input(INPUT_POST, 'optionsFinished', FILTER_SANITIZE_NUMBER_INT);
+$hand = filter_input(INPUT_POST, 'optionsHandedness', FILTER_SANITIZE_NUMBER_INT);
+$domStart = filter_input(INPUT_POST, 'sdh');
+$domEnd = filter_input(INPUT_POST, 'edh');
+$nonDomStart = filter_input(INPUT_POST, 'sndh');
+$nonDomEnd = filter_input(INPUT_POST, 'endh');
+$startFile = basename($_FILES["startImage"]["name"]);
+$endFile = basename($_FILES["endImage"]["name"]);
+$sentRelated = $_POST['relatedsigns'];
+$numOfAtt = filter_input(INPUT_POST, 'numberOfAttributes', FILTER_SANITIZE_NUMBER_INT);
+$attList = array();
+$attPropList = array();
 
 if ($inEd == "embr") {
-    $embr = filter_input(INPUT_POST, 'embrtext');
-    $s = new sign($sentSign);
-    $s->set_embr($embr);
-    $sh = new signHistoryClass($sentSign, $user, $embr);
-    $sh->insertWithOutDate();
-    $url = $s->updateEmbr();
+
+    $url = embrEd($sentSign, $embr, $user);
 } elseif ($inEd == "sign") {
-
-    $dbRelated = getRelatedSignArray($sentSign);
-
-    $gloss = filter_input(INPUT_POST, 'inputgloss', FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED);
-    $english = filter_input(INPUT_POST, 'inputenglish', FILTER_SANITIZE_STRING, FILTER_SANITIZE_ENCODED);
-    $asllvdLink = filter_input(INPUT_POST, 'inputasllvd');
-    $fish = filter_input(INPUT_POST, 'optionsFinished', FILTER_SANITIZE_NUMBER_INT);
-    $hand = filter_input(INPUT_POST, 'optionsHandedness', FILTER_SANITIZE_NUMBER_INT);
-    $domStart = filter_input(INPUT_POST, 'sdh');
-    $domEnd = filter_input(INPUT_POST, 'edh');
-    $nonDomStart = filter_input(INPUT_POST, 'sndh');
-    $nonDomEnd = filter_input(INPUT_POST, 'endh');
-    $startFile = basename($_FILES["startImage"]["name"]);
-    $endFile = basename($_FILES["endImage"]["name"]);
-    $sentRelated = $_POST['relatedsigns'];
-    $numOfAtt = filter_input(INPUT_POST, 'numberOfAttributes', FILTER_SANITIZE_NUMBER_INT);
-    $attList = array();
-    $attPropList = array();
 
 
     checkRelatedSigns($sentRelated, $dbRelated, $sentSign);
 
-    if (sizeof($relatedSign) == 0) {
-        $rsStat = "ok";
-    }
 //checking to see if there are active attributes
     if ($numOfAtt > 0) {
         //loop through the number of potential attributes
@@ -63,28 +56,40 @@ if ($inEd == "embr") {
     $s = new sign();
 }
 
-//header("Location: " . $url);
+header("Location: " . $url);
 
+function sgEdit() {
+    
+}
+
+function embrEd($_sentSign, $_embr, $_user) {
+    $s = new sign($_sentSign);
+    $s->set_embr($_embr);
+    $sh = new signHistoryClass($_sentSign, $_user, $_embr);
+    $sh->insertWithOutDate();
+    $ret = $s->updateEmbr();
+
+    return $ret;
+}
 
 function checkRelatedSigns($sentRelated, $dbRelated, $ssign) {
     if ($sentRelated != $dbRelated) {
         //adding new related signs
         foreach ($sentRelated as $sRel) {
             if (!in_array($sRel, $dbRelated)) {
-                $rs = new relatedSignClass($ssign,$sRel);
+                $rs = new relatedSignClass($ssign, $sRel);
                 $rs->insertRelatedSign();
                 //inserts the opposit way so both will have the relation
                 $rsOp = new relatedSignClass($sRel, $ssign);
                 $rsOp->insertRelatedSign();
             }
         }
-        
+
         //deleting signs that were no longer in the list 
-        foreach($dbRelated as $dbr){
+        foreach ($dbRelated as $dbr) {
             if (!in_array($dbr, $sentRelated)) {
-                $rs = new relatedSignClass($ssign,$dbr);
+                $rs = new relatedSignClass($ssign, $dbr);
                 $rs->removeRelatedSign();
-                
             }
         }
     }
